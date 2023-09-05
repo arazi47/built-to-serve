fe9fe28848da3d3808e40951d7bdb2a4cf988626
Alexis Razi <razialexis43@gmail.com>
2023-09-05 23:59:49 +0300

    Attempt to fix tests

--- tests/custom_content_parser/test_custom_content_parser.py
+++ tests/custom_content_parser/test_custom_content_parser.py
@@ -1,4 +1,4 @@
-from ws2g.custom_content_parser import get_custom_content_index
+from ws2g.custom_content_parser.custom_content_parser import get_custom_content_index
 
 
 def test_get_custom_content_index():
