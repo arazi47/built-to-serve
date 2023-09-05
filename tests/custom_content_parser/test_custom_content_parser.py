c6f3c5269fd6b97dbdff46710f0a7f568bc5c978
Alexis Razi <razialexis43@gmail.com>
2023-09-06 00:17:25 +0300

    Make tests run locally and on CI

--- tests/custom_content_parser/test_custom_content_parser.py
+++ tests/custom_content_parser/test_custom_content_parser.py
@@ -1,9 +1,17 @@
-from ws2g.custom_content_parser.custom_content_parser import get_custom_content_index
+# https://stackoverflow.com/a/34938623
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../ws2g")
+
+from custom_content_parser import get_custom_content_index  # noqa: E402
 
 
 def test_get_custom_content_index():
     expected_indices = [(23, 27), (38, 47)]
-    iterator = get_custom_content_index("test_file_input.html")
+    iterator = get_custom_content_index(
+        r"F:\repos\webserver-to-go\tests\custom_content_parser\test_file_input.html"
+    )
 
     for expected_cc_start_index, expected_cc_end_index in expected_indices:
         cc_start_index, cc_end_index = next(iterator)
