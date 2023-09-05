15aa4a35e6be0bc3a5724f397ea108bc91ffe3e2
Alexis Razi <razialexis43@gmail.com>
2023-09-05 23:43:57 +0300

    Add test_get_custom_content_index

---
+++ tests/custom_content_parser/test_custom_content_parser.py
@@ -0,0 +1,11 @@
+from ws2g.custom_content_parser import *
+
+def test_get_custom_content_index():
+    expected_indices = [(23, 27), (38, 47)]
+    iterator = get_custom_content_index("test_file_input.html")
+
+    for expected_cc_start_index, expected_cc_end_index in expected_indices:
+        cc_start_index, cc_end_index = next(iterator)
+        assert cc_start_index == expected_cc_start_index
+        assert cc_end_index == expected_cc_end_index
+    
