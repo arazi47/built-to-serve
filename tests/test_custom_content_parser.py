d6f6ab75851655ebec423e59c3d01836f9d56cca
Alexis Razi <razialexis43@gmail.com>
2023-09-06 21:12:19 +0300

    Mod

--- tests/test_custom_content_parser.py
+++ tests/test_custom_content_parser.py
@@ -1,16 +1,20 @@
 import os
-from ws2g.custom_content_parser import *
-# rom ws2g.custom_content_parser import get_custom_content_index
+# from ws2g.custom_content_parser import get_custom_content_index
 
 
 def test_get_custom_content_index():
-    expected_indices = [(23, 27), (38, 47)]
+    file = r"/opt/hostedtoolcache/Python/3.11.5/x64/lib/python3.11/site-packages/ws2g/custom_content_parser/"
+    with open(file, "r") as f:
+        for line in file:
+            print(line)
 
-    iterator = get_custom_content_index(
-        os.path.dirname(__file__) + "\\test_file_input.html"
-    )
+    # expected_indices = [(23, 27), (38, 47)]
 
-    for expected_cc_start_index, expected_cc_end_index in expected_indices:
-        cc_start_index, cc_end_index = next(iterator)
-        assert cc_start_index == expected_cc_start_index
-        assert cc_end_index == expected_cc_end_index
+    # iterator = get_custom_content_index(
+    #     os.path.dirname(__file__) + "\\test_file_input.html"
+    # )
+
+    # for expected_cc_start_index, expected_cc_end_index in expected_indices:
+    #     cc_start_index, cc_end_index = next(iterator)
+    #     assert cc_start_index == expected_cc_start_index
+    #     assert cc_end_index == expected_cc_end_index
