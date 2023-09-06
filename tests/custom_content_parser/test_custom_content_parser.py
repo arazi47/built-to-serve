6a81718ed4ae492b6274fff8073e0a12e259b0e6
Alexis Razi <razialexis43@gmail.com>
2023-09-06 19:30:01 +0300

    Mod back

--- tests/custom_content_parser/test_custom_content_parser.py
+++ tests/custom_content_parser/test_custom_content_parser.py
@@ -1,16 +1,15 @@
 import os
-from ws2g.custom_content_parser import get_lines_until_end
+from ws2g.custom_content_parser import get_custom_content_index
 
 
 def test_get_custom_content_index():
-    assert True
-    # expected_indices = [(23, 27), (38, 47)]
+    expected_indices = [(23, 27), (38, 47)]
 
-    # iterator = get_custom_content_index(
-    #     os.path.dirname(__file__) + "\\test_file_input.html"
-    # )
+    iterator = get_custom_content_index(
+        os.path.dirname(__file__) + "\\test_file_input.html"
+    )
 
-    # for expected_cc_start_index, expected_cc_end_index in expected_indices:
-    #     cc_start_index, cc_end_index = next(iterator)
-    #     assert cc_start_index == expected_cc_start_index
-    #     assert cc_end_index == expected_cc_end_index
+    for expected_cc_start_index, expected_cc_end_index in expected_indices:
+        cc_start_index, cc_end_index = next(iterator)
+        assert cc_start_index == expected_cc_start_index
+        assert cc_end_index == expected_cc_end_index
