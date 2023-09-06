917fe056d15103b45bef0769c32d8339b96d2685
Alexis Razi <razialexis43@gmail.com>
2023-09-06 21:15:29 +0300

    Mod

--- tests/test_custom_content_parser.py
+++ tests/test_custom_content_parser.py
@@ -1,20 +1,15 @@
 import os
-# from ws2g.custom_content_parser import get_custom_content_index
+from ws2g.custom_content_parser import get_custom_content_index
 
 
 def test_get_custom_content_index():
-    file = r"/opt/hostedtoolcache/Python/3.11.5/x64/lib/python3.11/site-packages/ws2g/custom_content_parser/custom_content_parser.py"
-    with open(file, "r") as f:
-        for line in f:
-            print(line)
+    expected_indices = [(23, 27), (38, 47)]
 
-    # expected_indices = [(23, 27), (38, 47)]
+    iterator = get_custom_content_index(
+        os.path.dirname(__file__) + "\\test_file_input.html"
+    )
 
-    # iterator = get_custom_content_index(
-    #     os.path.dirname(__file__) + "\\test_file_input.html"
-    # )
-
-    # for expected_cc_start_index, expected_cc_end_index in expected_indices:
-    #     cc_start_index, cc_end_index = next(iterator)
-    #     assert cc_start_index == expected_cc_start_index
-    #     assert cc_end_index == expected_cc_end_index
+    for expected_cc_start_index, expected_cc_end_index in expected_indices:
+        cc_start_index, cc_end_index = next(iterator)
+        assert cc_start_index == expected_cc_start_index
+        assert cc_end_index == expected_cc_end_index
