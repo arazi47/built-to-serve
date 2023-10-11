63dfeb44fed6f993d7b48a215f38e7e92a46d42d
Alexis Razi <razialexis43@gmail.com>
2023-10-11 20:34:31 +0300

    Refactor and add documentation

--- ws2g/main.py
+++ ws2g/main.py
@@ -4,16 +4,19 @@ import sys
 from urllib import parse
 from .views.views import (
     get_route_and_variables_for_path,
-    index_files_in_content,
+    index_files_in_content_dir,
 )
 from .views.view_helper import ViewHelper
 
 
 class Server(BaseHTTPRequestHandler):
     def send_status_and_headers(
-        self, content_type, status_code=200, additional_headers={}
+        self, content_type=None, status_code=200, additional_headers={}
     ):
-        """Send response, headers and end headers"""
+        """
+        Send response, headers and end headers
+        """
+
         self.send_response(status_code)
 
         headers = {
@@ -31,10 +34,81 @@ class Server(BaseHTTPRequestHandler):
         else:
             self.wfile.write(bytes(response, "utf-8"))
 
+    def _write_response(self, view_class, response_body, empty_response=False):
+        # self.send_status_and_headers(view_class.content_type)
+        # if not empty_response:
+        #     self.write_response(response_body, view_class.content_type)
+
+        # if response_body.content_type == "redirect":
+        #     self.redirect(response_body.file_path)
+        view_class
+        response_body
+        empty_response
+        # TODO
+
     def redirect(self, path):
-        self.send_response(301)
-        self.send_header("Location", path)
-        self.end_headers()
+        self.send_status_and_headers(
+            status_code=301, additional_headers={"Location": path}
+        )
+
+    def send_404_page_not_found(self):
+        """
+        Send a simple HTML page with the 404 status
+        saying that the requested page is not found.
+        """
+
+        self.send_status_and_headers("text/html", 404)
+        self.write_response("Page not found", "text/html")
+
+    def __get_func_argument_names(self, func):
+        return func.__code__.co_varnames[: func.__code__.co_argcount]
+
+    def __get_public_view_func_var_names_not_in_path(
+        self, view_func_arg_names, path_var_names
+    ):
+        """
+        Return a list of public view func var names (i.e. not starting with '__')
+        and that are not in path (i.e. '/users/<user_id>/update')
+        """
+
+        view_func_var_names = [
+            var_name
+            for var_name in view_func_arg_names
+            if var_name not in path_var_names and not var_name.startswith("__")
+        ]
+
+        return view_func_var_names
+
+    def __get_request_fields(self):
+        """
+        Return an HTTP request fields.
+        Usually has data when a POST request came in.
+        """
+
+        length = int(self.headers.get("content-length"))
+        field_data = self.rfile.read(length)
+        fields = parse.parse_qs(str(field_data, "UTF-8"), keep_blank_values=True)
+
+        return fields
+
+    def __get_public_view_func_var_values_not_in_path(
+        self, request_fields, view_func_var_names
+    ):
+        return [request_fields[var_name][0] for var_name in view_func_var_names]
+
+    def __get_extra_fields(self, request_fields, view_func_var_names):
+        """
+        In case there are any fields contained in the POST request
+        which are unused by the client in view_func, they can still
+        have access to them using the '__extra_fields' argument.
+        """
+
+        extra_fields = {}
+        for field, value in request_fields.items():
+            if field not in view_func_var_names:
+                extra_fields[field] = value
+
+        return extra_fields
 
     def do_POST(self):
         try:
@@ -44,31 +118,44 @@ class Server(BaseHTTPRequestHandler):
                 path_var_values,
             ) = get_route_and_variables_for_path(self.path[1:], "POST")
         except KeyError as e:
-            self.send_status_and_headers("text/html", 404)
-            self.write_response("Page not found", "text/html")
+            self.send_404_page_not_found()
 
             print(e)
             return
 
-        view_func_var_names = [
-            var_name
-            for var_name in view_class.view_func.__code__.co_varnames[
-                : view_class.view_func.__code__.co_argcount
-            ]
-            if var_name not in path_var_names
-        ]
-        length = int(self.headers.get("content-length"))
-        field_data = self.rfile.read(length)
-        fields = parse.parse_qs(str(field_data, "UTF-8"), keep_blank_values=True)
-        rest_of_view_func_var_values = [
-            fields[var_name][0] for var_name in view_func_var_names
-        ]
+        request_fields = self.__get_request_fields()
+        view_func_arg_names = self.__get_func_argument_names(view_class.view_func)
+        public_view_func_var_names_not_in_path = (
+            self.__get_public_view_func_var_names_not_in_path(
+                view_func_arg_names, path_var_names
+            )
+        )
+        public_view_func_var_values_not_in_path = (
+            self.__get_public_view_func_var_values_not_in_path(
+                request_fields, public_view_func_var_names_not_in_path
+            )
+        )
 
         # The path may be admin/users/1234/edit
         # path_var_values will be [1234] - user id
-        # rest_of_view_func_var_values will be whatever the user passed as input,
-        # e.g. the new user spassword or whatever the admin edited
-        response = view_class.view_func(*path_var_values, *rest_of_view_func_var_values)
+        # public_view_func_var_values_not_in_path will be whatever the user
+        # passed as input, e.g. the new user's password
+        view_func_arg_values = [
+            *path_var_values,
+            *public_view_func_var_values_not_in_path,
+        ]
+
+        if "__extra_fields" in view_class.view_func.__code__.co_varnames:
+            extra_fields = self.__get_extra_fields(
+                request_fields,
+                public_view_func_var_names_not_in_path,
+            )
+
+            response = view_class.view_func(
+                *view_func_arg_values, __extra_fields=extra_fields
+            )
+        else:
+            response = view_class.view_func(*view_func_arg_values)
 
         if response.content_type == "redirect":
             self.redirect(response.file_path)
@@ -82,8 +169,7 @@ class Server(BaseHTTPRequestHandler):
                 self.path[1:], "GET"
             )
         except KeyError as e:
-            self.send_status_and_headers("text/html", 404)
-            self.write_response("Page not found", "text/html")
+            self.send_404_page_not_found()
 
             print(e)
             return
@@ -104,8 +190,7 @@ def run():
         server_address = str(sys.argv[1])
         server_port = int(sys.argv[2])
 
-    index_files_in_content()
-
+    index_files_in_content_dir()
     server = HTTPServer((server_address, server_port), Server)
 
     try:
--- ws2g/views/views.py
+++ ws2g/views/views.py
@@ -7,13 +7,17 @@ from ws2g.views.view_helper import ViewHelper
 
 CONTENT_DIRECTORY_NAME = "content"
 
-# Stores all user defined routes and
-# all routes created from files inside CONTENT_DIRECTORY_NAME
+"""
+Stores all user defined routes and files inside CONTENT_DIRECTORY_NAME.
+"""
 content_routes = {}
 
 
 def has_variable_form(string):
-    """Returns True if string has the form <var>"""
+    """
+    Returns True if string is of the form '<var>'
+    """
+
     if len(string) < 3:
         return False
 
@@ -26,22 +30,30 @@ def has_variable_form(string):
     return string.isidentifier()
 
 
-def get_route_and_variables_for_path(path, method):
-    """Return the correct function to execute for given path
-    along with the extracted variables."""
+def get_route_and_variables_for_path(request_path, method):
+    """
+    Return the view_func, variable names and variable values
+    for a given path, if one exists.
+
+    content_routes entry: "/thing/<var1>/asd/<var2>/etc"
+    request_path: "/thing/hithere/asd/12/etc"
+
+    Return for the above example: <func>, ['var1', 'var2'], ['hithere', '12']
+
+    If request_path does not match with any preexisting identifier in content_routes,
+    an error is raised.
+    """
+
     var_names = []
     var_values = []
 
-    if path in content_routes:
-        method_view_dict = content_routes[path]
+    if request_path in content_routes:
+        method_view_dict = content_routes[request_path]
         if method in method_view_dict:
             return method_view_dict[method], var_names, var_values
 
-    # content_routes entry: "/thing/<var1>/asd/<var2>/etc"
-    # path: "/thing/hithere/asd/12/etc"
-
-    original_path = path
-    path = path.split("/")
+    original_request_path = request_path
+    request_path = request_path.split("/")
     for route_identifier in content_routes.keys():
         if method not in content_routes[route_identifier]:
             continue
@@ -52,8 +64,8 @@ def get_route_and_variables_for_path(path, method):
         identifier_contains_vars = False
 
         # If they split the same
-        if len(path) == len(route_identifier):
-            for path_elem, identifier_elem in zip(path, route_identifier):
+        if len(request_path) == len(route_identifier):
+            for path_elem, identifier_elem in zip(request_path, route_identifier):
                 identifier_contains_vars = False
                 if has_variable_form(identifier_elem):
                     if not identifier_contains_vars:
@@ -80,12 +92,26 @@ def get_route_and_variables_for_path(path, method):
 
     raise KeyError(
         "Exception for path="
-        + original_path
+        + original_request_path
         + ". The path is either not routed correctly or not created at all."
     )
 
 
 class View:
+    """
+    Class wrapper for a 'view_func'.
+
+    'file_path' is either either a relative path inside the content directory
+    or an identifier that was created using the 'route' decorator.
+
+    'content_type' is a string that will be sent in a response's header.
+
+    'view_func' is the function that will be called when a client request
+    matches 'file_path'.
+
+    'method' is a string that represents whether the request is a GET or a POST.
+    """
+
     def __init__(self, file_path, content_type, view_func, method) -> None:
         self.file_path = file_path
         self.content_type = content_type
@@ -94,13 +120,18 @@ class View:
 
 
 def route(identifier, method):
+    """
+    Create a route, i.e. map an identifier to a 'view_func' (the 'method' parameter).
+    This function is expected to be used as a decorator.
+    The identifier input is not validated.
+    """
+
     def wrapper(view_func):
         # Get rid of leading '/'
         id = identifier[1:]
-        if id in content_routes:
-            content_routes[id][method] = View(None, "text/html", view_func, method)
-        else:
-            content_routes[id] = {method: View(None, "text/html", view_func, method)}
+        content_routes[id] = content_routes.get(
+            id, {method: View(None, "text/html", view_func, method)}
+        )
         return view_func
 
     return wrapper
@@ -108,10 +139,9 @@ def route(identifier, method):
 
 def render(path, template_data=dict()):
     """
-    path can either be a full path, in which case just call view_func()
-    or an identifier.
+    Render a webpage without redirecting the client.
 
-    TODO handle content routes with variables
+    'path' refers to a preexisting file in the content dir
     """
 
     relative_path = CONTENT_DIRECTORY_NAME + "/" + path
@@ -127,17 +157,27 @@ def render(path, template_data=dict()):
     elif path[1:] in content_routes:
         method_view_dict = content_routes[path[1:]]
         # Can't render a POST
-        if "GET" in method_view_dict:
+        try:
             return method_view_dict["GET"].view_func()
-
-    raise KeyError('Page "' + path + '" not found!')
+        except KeyError:
+            raise KeyError('Page "' + path + '" not found!')
 
 
 def redirect(path):
+    """
+    Return a special View, where content_type="redirect".
+    This will redirect the client to the identifier specified
+    in variable 'path', generating a GET request.
+    """
+
     return View(path, "redirect", None, None)
 
 
-def index_files_in_content(full_path_to_content_dir=""):
+def index_files_in_content_dir(full_path_to_content_dir=""):
+    """
+    Load preexisting files in the content directory in content_routes
+    """
+
     # https://www.geeksforgeeks.org/why-do-python-lambda-defined-in-a-loop-with-different-values-all-return-the-same-result/
     def create_lambda(func, *args):
         return lambda: func(*args)
