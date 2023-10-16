eaf0a7e38f35dd2ba84cf1ecb542a675e053bd5f
Alexis Razi <razialexis43@gmail.com>
2023-10-16 22:48:04 +0300

    Lint

--- tests/routing/test_routing.py
+++ tests/routing/test_routing.py
@@ -4,8 +4,7 @@ from ws2g.views.views import content_routes, index_files_in_content_dir
 
 def test_htm_html_php_files_are_not_indexed_by_default():
     assert content_routes == {}
-    
+
     index_files_in_content_dir(os.path.dirname(__file__))
 
     assert content_routes == {}
-
